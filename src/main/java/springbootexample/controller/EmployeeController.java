d7d404b51e213e96e4b74bc146426c951437bed9
IamGiel <lfagel84@gmail.com>
2018-08-10 14:04:11 -0400

    added controller

--- src/main/java/springbootexample/controller/EmployeeController.java
+++ src/main/java/springbootexample/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package springbootexample.controller;
 
-public class EmployeeController {
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springbootexample.dao.EmployeeDao;
+import springbootexample.model.Employee;
+
+//this is where all the rest api calls live
+
+@RestController
+@RequestMapping("/company")
+public class EmployeeController {
+	@Autowired
+	EmployeeDao employeeDao
+	
+//	save employee
+	@PostMapping("/employees")
+	public Employee createEmployee(@Valid @RequestBody Employee emp) {
+	
+	}
 }
